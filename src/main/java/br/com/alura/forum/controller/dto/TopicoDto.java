b052799986154bb610cc20a44b40114c2c401fb6
unknown <gersonsouza97@outlook.com>
2021-02-18 10:47:32 -0300

    adicionando no repositorio

---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,48 @@
+package br.com.alura.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+
+import br.com.alura.forum.modelo.Topico;
+
+public class TopicoDto {
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime datacriacao;
+	
+	
+	public TopicoDto(Topico topico) {
+		super();
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.datacriacao = topico.getDataCriacao();
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public LocalDateTime getDatacriacao() {
+		return datacriacao;
+	}
+
+
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
+	
+		//return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+		return topicos.map(TopicoDto::new);
+	}
+	
+	
+}
